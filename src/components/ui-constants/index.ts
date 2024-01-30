16aa9fe40e95450a34f077220d05b70b1eb425b8
Michael Olawale <tech@mykelcodes.com>
2024-01-30 20:46:50 +0000

    more colors

--- src/components/ui-constants/index.ts
+++ src/components/ui-constants/index.ts
@@ -5,4 +5,10 @@ export enum BrandColors {
     gray = '#F1F1F1',
     darkGray = '#B0ADAF',
     red = '#CA0000',
+    primaryHover = '#00AE6D',
+    primaryActive = '#008251',
+    grayHover = '#F8F8F8',
+    grayActive = '#E3E3E3',
+    blackHover = '#261A21',
+    blackActive = '#44383F',
 }
